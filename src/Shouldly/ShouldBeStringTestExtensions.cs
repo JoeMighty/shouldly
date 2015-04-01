c2235cd4e0a43b252887cc0ed6be3ad28177012f
JoeMighty <josephwoodward@xeuse.com>
2015-04-01 14:03:23 +0100

    Started on ShouldBeStringTestExtensions

--- src/Shouldly/ShouldBeStringTestExtensions.cs
+++ src/Shouldly/ShouldBeStringTestExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 
 namespace Shouldly
@@ -11,9 +12,19 @@ namespace Shouldly
         /// </summary>
         public static void ShouldBe(this string actual, string expected, Case caseSensitivity)
         {
+            ShouldBe(actual, expected, caseSensitivity, () => null);
+        }
+
+        public static void ShouldBe(this string actual, string expected, Case caseSensitivity, string customMessage)
+        {
+            ShouldBe(actual, expected, caseSensitivity, () => customMessage);
+        }
+
+        public static void ShouldBe(this string actual, string expected, Case caseSensitivity, Func<string> customMessage)
+        {
             actual.AssertAwesomely(v => (caseSensitivity == Case.Sensitive)
                 ? Is.Equal(v, expected)
-                : Is.StringEqualIgnoreCase(v, expected), actual, expected);
+                : Is.StringEqualIgnoreCase(v, expected), actual, expected, customMessage);
         }
 
         /// <summary>
