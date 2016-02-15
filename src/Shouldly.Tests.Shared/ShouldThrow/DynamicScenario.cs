8ed1db47d9ad79086456700c300edea1c6caad17
Joseph Woodward <josephwoodward@xeuse.com>
2016-02-15 02:04:27 +0000

    Initial commit

---
+++ src/Shouldly.Tests.Shared/ShouldThrow/DynamicScenario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Shouldly.Tests.Strings;
+using Xunit;
+
+namespace Shouldly.Tests.ShouldThrow
+{
+    public class DynamicScenario
+    {
+        public void Thrower()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void DynamicDelegateScenarioShouldFail()
+        {
+            Action action = () => { };
+            Verify.ShouldFail(() =>
+                action.ShouldThrow<NotImplementedException>("Some additional context"),
+                errorWithSource:
+                    @"`action()`
+    should throw
+System.NotImplementedException
+    but did not
+
+Additional Info:
+    Some additional context",
+                errorWithoutSource:
+                    @"delegate
+    should throw
+System.NotImplementedException
+    but did not
+
+Additional Info:
+    Some additional context");
+        }
+
+        [Fact]
+        public void ShouldPass()
+        {
+            Should.Throw<NotImplementedException>(() => ((dynamic)this).Thrower());
+        }
+    }
+}
